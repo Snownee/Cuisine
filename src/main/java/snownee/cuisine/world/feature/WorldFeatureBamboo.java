b1deb51f3c081d6f4ec27827220efecfd9be95c4
Snownee <1850986885@qq.com>
2018-10-07 17:18:45 +0800

    Optimize imports

--- src/main/java/snownee/cuisine/world/feature/WorldFeatureBamboo.java
+++ src/main/java/snownee/cuisine/world/feature/WorldFeatureBamboo.java
@@ -3,7 +3,6 @@ package snownee.cuisine.world.feature;
 import java.util.Random;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
