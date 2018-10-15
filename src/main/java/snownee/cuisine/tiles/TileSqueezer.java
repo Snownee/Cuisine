16cb64ca905a3b568d8368c6e194109cd7b8dec4
3TUSK <urey.s.knowledge@gmail.com>
2018-10-15 16:43:24 -0700

    So here is progress: squeezer animation, random extending version. Potential NSFW alert.

--- src/main/java/snownee/cuisine/tiles/TileSqueezer.java
+++ src/main/java/snownee/cuisine/tiles/TileSqueezer.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.BlockDispenser;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.animation.Animation;
 import net.minecraftforge.common.animation.TimeValues;
@@ -14,26 +15,90 @@ import snownee.cuisine.Cuisine;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Locale;
 
-public class TileSqueezer extends TileBase
+public class TileSqueezer extends TileBase implements ITickable
 {
 
+    /**
+     * Private enum used for denoting the "piston arm" state of squeezer.
+     */
+    enum State
+    {
+        EXTRACTED, EXTENDING, EXTENDED, EXTRACTING;
+
+        final String stateName;
+
+        State()
+        {
+            stateName = this.name().toLowerCase(Locale.ENGLISH);
+        }
+    }
+
+    /**
+     * The farthest offset that "piston arm" can reach. Negative means it moves
+     * on negative direction, and in this case it is down direction (negative
+     * y-axis).
+     */
+    private static final float OFFSET_LIMIT = -0.78125F;
+
+    /**
+     * Location of animation state machine definition file used for squeezer.
+     */
     private static final ResourceLocation STATE_MACHINE = new ResourceLocation(Cuisine.MODID, "asms/squeezer.json");
 
     private IAnimationStateMachine stateMachine;
 
-    private TimeValues.VariableValue magic = new TimeValues.VariableValue(0F);
+    private TimeValues.VariableValue extensionOffset = new TimeValues.VariableValue(0F);
+
+    private int extensionProgress;
 
     private boolean triggered;
 
+    private State state = State.EXTRACTED;
+
     public TileSqueezer()
     {
-        this.stateMachine = Cuisine.proxy.loadAnimationStateMachine(STATE_MACHINE, ImmutableMap.of("magic", this.magic));
+        this.stateMachine = Cuisine.proxy.loadAnimationStateMachine(STATE_MACHINE, ImmutableMap.of("offset", this.extensionOffset));
     }
 
     public void onTriggered(boolean triggered)
     {
         this.triggered = triggered;
+    }
+
+    @Override
+    public void update()
+    {
+        extensionOffset.setValue(this.world.rand.nextInt(100) / 100F * OFFSET_LIMIT);
+        /*
+        switch (state)
+        {
+            case EXTENDING:
+            {
+                //stateMachine.transition(State.EXTENDED.stateName);
+            }
+            case EXTRACTING:
+            {
+                //stateMachine.transition(State.EXTRACTED.stateName);
+            }
+        }*/
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        this.extensionProgress = compound.getInteger("Extension");
+        this.state = State.values()[compound.getInteger("state")];
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        compound.setInteger("Extension", this.extensionProgress);
+        compound.setInteger("state", this.state.ordinal());
+        return super.writeToNBT(compound);
     }
 
     @Override
@@ -44,13 +109,13 @@ public class TileSqueezer extends TileBase
         {
             if ("extracted".equals(stateMachine.currentState()))
             {
-                magic.setValue(Animation.getWorldTime(this.getWorld()));
+                extensionOffset.setValue(Animation.getWorldTime(this.getWorld()));
                 stateMachine.transition("extending");
             }
         }
         else
         {
-            magic.setValue(Animation.getWorldTime(this.getWorld()));
+            extensionOffset.setValue(Animation.getWorldTime(this.getWorld()));
             stateMachine.transition("extracting");
         }
     }
