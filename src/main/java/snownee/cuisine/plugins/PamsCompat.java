981f3a817f9f5cac6ae650e37a2d4daf826fc7c5
3TUSK <urey.s.knowledge@gmail.com>
2018-10-18 22:53:54 -0700

    Fix build (read "adapt to current internal API design")

--- src/main/java/snownee/cuisine/plugins/PamsCompat.java
+++ src/main/java/snownee/cuisine/plugins/PamsCompat.java
@@ -8,6 +8,7 @@ import net.minecraftforge.oredict.OreDictionary;
 import snownee.cuisine.Cuisine;
 import snownee.cuisine.api.CulinaryHub;
 import snownee.cuisine.api.Form;
+import snownee.cuisine.api.Ingredient;
 import snownee.cuisine.api.Material;
 import snownee.cuisine.api.MaterialCategory;
 import snownee.cuisine.api.prefab.SimpleMaterialImpl;
@@ -22,11 +23,11 @@ public class PamsCompat implements IModule
     public void init()
     {
         Material blackberry = CulinaryHub.API_INSTANCE.register(new SimpleMaterialImpl("blackberry", 0x1D1019, 0, 0, 0, 0, 0, MaterialCategory.FRUIT).setValidForms(Form.JUICE_ONLY));
-        CuisineInternalGateway.INSTANCE.oreDictToMaterialMapping.put("cropBlackberry", blackberry);
+        CuisineInternalGateway.INSTANCE.oreDictIngredients.put("cropBlackberry", new Ingredient(blackberry, Form.FULL, 1));
         Material blueberry = CulinaryHub.API_INSTANCE.register(new SimpleMaterialImpl("blueberry", 0x204569, 0, 0, 0, 0, 0, MaterialCategory.FRUIT).setValidForms(Form.JUICE_ONLY));
-        CuisineInternalGateway.INSTANCE.oreDictToMaterialMapping.put("cropBlueberry", blueberry);
+        CuisineInternalGateway.INSTANCE.oreDictIngredients.put("cropBlueberry", new Ingredient(blueberry, Form.FULL, 1));
         Material grape = CulinaryHub.API_INSTANCE.register(new SimpleMaterialImpl("grape", 0x582945, 0, 0, 0, 0, 0, MaterialCategory.FRUIT).setValidForms(Form.JUICE_ONLY));
-        CuisineInternalGateway.INSTANCE.oreDictToMaterialMapping.put("cropGrape", grape);
+        CuisineInternalGateway.INSTANCE.oreDictIngredients.put("cropGrape", new Ingredient(grape, Form.FULL, 1));
     }
 
     @Override
