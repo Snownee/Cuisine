d359cf0a03280dfc3be77da7ac68a86f34df8e87
3TUSK <urey.s.knowledge@gmail.com>
2018-10-18 22:54:32 -0700

    Uh. Priority-based comparator. Used for... things. Like sorting things.

--- src/main/java/snownee/cuisine/api/Effect.java
+++ src/main/java/snownee/cuisine/api/Effect.java
@@ -7,8 +7,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.Comparator;
+
 public interface Effect
 {
+
+    Comparator<Effect> PRIORITY_COMPARATOR = Comparator.comparingInt(Effect::getPriority);
+
+    Comparator<Effect> INVERSE_PRIORITY_COMPARATOR = PRIORITY_COMPARATOR.reversed();
+
     void onEaten(ItemStack stack, EntityPlayer player, CompositeFood compositeFood, @Nullable Ingredient ingredient, EffectCollector collector);
 
     int getPriority();
