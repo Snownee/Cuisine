c4591d9eed6bcedeeb5e3dfbbd434bf599d47825
3TUSK <urey.s.knowledge@gmail.com>
2018-10-21 12:35:27 -0700

    Update Forge to 14.23.5.2772 so we have access to the `forge:default-block` transform

--- src/main/java/snownee/cuisine/client/model/ChoppingBoardOverride.java
+++ src/main/java/snownee/cuisine/client/model/ChoppingBoardOverride.java
@@ -14,7 +14,9 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ForgeBlockStateV1;
 import net.minecraftforge.client.model.PerspectiveMapWrapper;
+import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
 import net.minecraftforge.common.util.Constants;
 import snownee.cuisine.util.ItemNBTUtil;
@@ -22,6 +24,7 @@ import snownee.cuisine.util.ItemNBTUtil;
 import javax.annotation.Nullable;
 import javax.vecmath.Vector3f;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -37,71 +40,35 @@ public final class ChoppingBoardOverride extends ItemOverrideList
      */
     static final TRSRTransformation CHOPPING_BOARD_SCALE_DOWN = new TRSRTransformation(new Vector3f(0F, -.38F, 0F), null, new Vector3f(0.75F, 0.25F, 0.75F), null);
 
-    /*
-     * The correct TRSRTransformation data for correctly rendering a chopping board
-     * as an item. The core data is cited from Connected Texture Mod (CTM) with
-     * permission from tterrag. In addition to those data, we do an additional
-     * transform in order to make them look like a "board". We also call
-     * blockCenterToCorner to make sure everything is sane for an ItemBlock.
-     *
-     * Permission:
-     * http://tritusk.info/pics/tterrag-permission-default-block-transform-data.jpg
-     *
-     * Reference:
-     * https://github.com/Chisel-Team/ConnectedTexturesMod/blob/1.10/dev/src/main/
-     * java/team/chisel/ctm/client/model/AbstractCTMBakedModel.java#L245-L253
-     *
-     * TODO: Awaiting bs2609's Pull Request (GitHub MinecraftForge/MinecraftForge#5180)
-     */
-    private static final ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> CHOPPING_BOARD_TRANSFORMS
-            = ImmutableMap.<ItemCameraTransforms.TransformType, TRSRTransformation>builder()
-            .put(ItemCameraTransforms.TransformType.GUI, TRSRTransformation.blockCenterToCorner(
-                    of(0, 0, 0, 30, 45, 0, 0.625f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, TRSRTransformation.blockCenterToCorner(
-                    of(0, 2.5f, 0, 75, 45, 0, 0.375f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, TRSRTransformation.blockCenterToCorner(
-                    of(0, 2.5f, 0, 75, 45, 0, 0.375f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND, TRSRTransformation.blockCenterToCorner(
-                    of(0, 0, 0, 0, 45, 0, 0.4f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND, TRSRTransformation.blockCenterToCorner(
-                    of(0, 0, 0, 0, 225, 0, 0.4f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.GROUND, TRSRTransformation.blockCenterToCorner(
-                    of(0, 2, 0, 0, 0, 0, 0.25f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .put(ItemCameraTransforms.TransformType.FIXED, TRSRTransformation.blockCenterToCorner(
-                    of(0, 0, 0, 0, 0, 0, 0.5f).compose(CHOPPING_BOARD_SCALE_DOWN)
-            ))
-            .build();
-
     /**
-     * Get a {@code TRSRTransformation} object using supplied data. Only uniform scale is supported.
-     * This is NOT a general purpose short-cut for getting a TRSRTransformation! It's here for mere
-     * purpose of being a human-readable shortcut.
-     *
-     * @param tx Translation x in pixel
-     * @param ty Translation y in pixel
-     * @param tz Translation z in pixel
-     * @param ax Rotation angle x in degree
-     * @param ay Rotation angle y in degree
-     * @param az Rotation angle z in degree
-     * @param scale Uniform scale quantity
-     *
-     * @return The correct TRSRTransformation object
+     * The correct TRSRTransformation data for correctly rendering a chopping board
+     * as an item.
      */
-    @SuppressWarnings("All") // Hush, IDEA
-    private static TRSRTransformation of(float tx, float ty, float tz, float ax, float ay, float az, float scale)
+    private static final ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> CHOPPING_BOARD_TRANSFORMS;
+
+    static
     {
-        return new TRSRTransformation(
-                new Vector3f(tx / 16, ty / 16, tz / 16), // translation
-                TRSRTransformation.quatFromXYZDegrees(new Vector3f(ax, ay, az)), // left-rotation
-                new Vector3f(scale, scale, scale), // uniform scale
-                null // we don't do right rotation here
-        );
+        ImmutableMap.Builder<ItemCameraTransforms.TransformType, TRSRTransformation> builder = ImmutableMap.builder();
+        Optional<IModelState> defaultBlockTransform = ForgeBlockStateV1.Transforms.get("forge:default-block");
+        if (defaultBlockTransform.isPresent())
+        {
+            IModelState modelState = defaultBlockTransform.get();
+            TRSRTransformation extraTransform = TRSRTransformation.blockCenterToCorner(CHOPPING_BOARD_SCALE_DOWN);
+            for (ItemCameraTransforms.TransformType transformType : ItemCameraTransforms.TransformType.values())
+            {
+                Optional<TRSRTransformation> result = modelState.apply(Optional.of(transformType));
+                if (result.isPresent())
+                {
+                    TRSRTransformation actualTransform = result.get();
+                    builder.put(transformType, actualTransform.compose(extraTransform));
+                }
+            }
+            CHOPPING_BOARD_TRANSFORMS = builder.build();
+        }
+        else
+        {
+            throw new NullPointerException("Transform 'forge:default-block' does not exist. In theory this should never happen.");
+        }
     }
 
     private final Cache<ItemStack, IBakedModel> modelCache = CacheBuilder.newBuilder()
