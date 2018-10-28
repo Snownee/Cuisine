c86e6d2f3dcadfa5a8903f34e6320a7c86c07ec4
3TUSK <urey.s.knowledge@gmail.com>
2018-10-27 23:46:55 -0700

    De-final fields in CuisineFluidBlocks. They are auto-updated if someone uses registry override; if someone manually tamper it... that's their problem.

--- src/main/java/snownee/cuisine/fluids/CuisineFluidBlocks.java
+++ src/main/java/snownee/cuisine/fluids/CuisineFluidBlocks.java
@@ -5,45 +5,35 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import snownee.cuisine.Cuisine;
 
 @GameRegistry.ObjectHolder(Cuisine.MODID)
-public class CuisineFluidBlocks
+public final class CuisineFluidBlocks
 {
-
-    // TODO (3TUSK): Consider de-final these fields? Normal add-ons should NOT touch internal anyway.
+    private CuisineFluidBlocks()
+    {
+        throw new UnsupportedOperationException("No instance for you");
+    }
 
     @GameRegistry.ObjectHolder("soy_milk")
-    public static final BlockFluidBase SOY_MILK;
+    public static BlockFluidBase SOY_MILK;
 
     @GameRegistry.ObjectHolder("milk")
-    public static final BlockFluidBase MILK;
+    public static BlockFluidBase MILK;
 
     @GameRegistry.ObjectHolder("soy_sauce")
-    public static final BlockFluidBase SOY_SAUCE;
+    public static BlockFluidBase SOY_SAUCE;
 
     @GameRegistry.ObjectHolder("rice_vinegar")
-    public static final BlockFluidBase RICE_VINEGAR;
+    public static BlockFluidBase RICE_VINEGAR;
 
     @GameRegistry.ObjectHolder("fruit_vinegar")
-    public static final BlockFluidBase FRUIT_VINEGAR;
+    public static BlockFluidBase FRUIT_VINEGAR;
 
     @GameRegistry.ObjectHolder("sesame_oil")
-    public static final BlockFluidBase SESAME_OIL;
+    public static BlockFluidBase SESAME_OIL;
 
     @GameRegistry.ObjectHolder("edible_oil")
-    public static final BlockFluidBase EDIBLE_OIL;
+    public static BlockFluidBase EDIBLE_OIL;
 
     @GameRegistry.ObjectHolder("sugarcane_juice")
-    public static final BlockFluidBase SUGARCANE_JUICE;
-
-    static // Hack around things to get rid of IDEA warning.
-    {
-        SOY_MILK = null;
-        MILK = null;
-        SOY_SAUCE = null;
-        RICE_VINEGAR = null;
-        FRUIT_VINEGAR = null;
-        SESAME_OIL = null;
-        EDIBLE_OIL = null;
-        SUGARCANE_JUICE = null;
-    }
+    public static BlockFluidBase SUGARCANE_JUICE;
 
 }
