06bd667e036f3028b44b023745b4387312809d48
3TUSK <urey.s.knowledge@gmail.com>
2018-11-10 21:57:46 -0800

    VillagerHandler: null-safety; don't import nested class for consisted style; use Arrays.asList & Collections.singletonList to replace the usage of guava ImmutableList.
    (off-topic, personal opinion: I wish we have access to Java 9's `List.of(...)`.)
    
    TODO: Do we need logging when the farmer profession is missing due to whatever reason?

--- src/main/java/snownee/cuisine/events/VillagerHandler.java
+++ src/main/java/snownee/cuisine/events/VillagerHandler.java
@@ -1,11 +1,10 @@
 package snownee.cuisine.events;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
-
-import com.google.common.collect.ImmutableList;
 
 import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.passive.EntityVillager;
@@ -17,8 +16,7 @@ import net.minecraft.village.MerchantRecipeList;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.VillagerRegistry.VillagerCareer;
-import net.minecraftforge.fml.common.registry.VillagerRegistry.VillagerProfession;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
 import snownee.cuisine.Cuisine;
 import snownee.cuisine.CuisineRegistry;
 import snownee.cuisine.blocks.BlockCuisineCrops;
@@ -31,14 +29,19 @@ import snownee.kiwi.util.definition.ItemDefinition;
 public class VillagerHandler
 {
     @SubscribeEvent
-    public static void onRegisterVillagers(RegistryEvent.Register<VillagerProfession> event)
+    public static void onRegisterVillagers(RegistryEvent.Register<VillagerRegistry.VillagerProfession> event)
     {
-        VillagerProfession profession = event.getRegistry().getValue(new ResourceLocation("minecraft:farmer"));
-        VillagerCareer career = profession.getCareer(0); // farmer
+        VillagerRegistry.VillagerProfession profession = event.getRegistry().getValue(new ResourceLocation("minecraft:farmer"));
+        if (profession == null)
+        {
+            // Skip this if (for whatever reason) it is null.
+            return;
+        }
+        VillagerRegistry.VillagerCareer career = profession.getCareer(0); // farmer
 
         // career.addTrade(2, new TradeCrops(ItemDefinition.of(CuisineRegistry.LIFE_ESSENCE), 4));
         career.addTrade(3, new TradeCrops(ItemDefinition.of(CuisineRegistry.BASIC_FOOD, ItemBasicFood.Variants.TOFU.getMeta()), 8));
-        career.addTrade(4, new TradeCrops(ImmutableList.of(ItemDefinition.of(CuisineRegistry.CROPS, ItemCrops.Variants.CHILI.getMeta()), ItemDefinition.of(CuisineRegistry.CROPS, ItemCrops.Variants.GARLIC.getMeta())), 3));
+        career.addTrade(4, new TradeCrops(Arrays.asList(ItemDefinition.of(CuisineRegistry.CROPS, ItemCrops.Variants.CHILI.getMeta()), ItemDefinition.of(CuisineRegistry.CROPS, ItemCrops.Variants.GARLIC.getMeta())), 3));
 
         List<ItemDefinition> crops = Arrays.stream(WorldGenGarden.PLANT_POOL).filter(b -> b instanceof BlockCuisineCrops).map(b -> (((BlockCuisineCrops) b).getCrop())).collect(Collectors.toList());
         career.addTrade(2, new TradeCrops(crops, -17));
@@ -53,7 +56,7 @@ public class VillagerHandler
 
         public TradeCrops(ItemDefinition stack, int count)
         {
-            this(ImmutableList.of(stack), count);
+            this(Collections.singletonList(stack), count);
         }
 
         public TradeCrops(List<ItemDefinition> stacks, int count)
