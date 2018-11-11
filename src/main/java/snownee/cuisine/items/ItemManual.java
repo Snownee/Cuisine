77b085f517a6617bdc162afc8ac55b757f01fd55
3TUSK <urey.s.knowledge@gmail.com>
2018-11-11 13:34:26 -0800

    No-op-ify the old manual; remove library.book.Element because it is now obsolete.

--- src/main/java/snownee/cuisine/items/ItemManual.java
+++ src/main/java/snownee/cuisine/items/ItemManual.java
@@ -2,13 +2,11 @@ package snownee.cuisine.items;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import snownee.cuisine.Cuisine;
-import snownee.cuisine.client.gui.CuisineGUI;
 import snownee.kiwi.item.ItemMod;
 
 public class ItemManual extends ItemMod
@@ -22,20 +20,13 @@ public class ItemManual extends ItemMod
     }
 
     @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
     {
-        ItemStack stack = playerIn.getHeldItem(handIn);
-
-        if (!worldIn.isRemote)
-        {
-            // this.resolveContents(itemstack, playerIn);
-        }
-
-        playerIn.openGui(Cuisine.getInstance(), CuisineGUI.MANUAL, worldIn, handIn == EnumHand.MAIN_HAND
-                ? playerIn.inventory.currentItem
-                : playerIn.inventory.mainInventory.size() + playerIn.inventory.armorInventory.size(), 0, 0);
-        playerIn.addStat(StatList.getObjectUseStats(this));
-        return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+        ItemStack heldThing = player.getHeldItem(hand);
+        /*player.openGui(Cuisine.getInstance(), CuisineGUI.MANUAL, world, hand == EnumHand.MAIN_HAND
+                ? player.inventory.currentItem
+                : player.inventory.mainInventory.size() + player.inventory.armorInventory.size(), 0, 0);*/
+        return new ActionResult<>(EnumActionResult.SUCCESS, heldThing);
     }
 
 }
