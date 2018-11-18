55dc86ce3e774b376e9052f427014318f7bdafcc
Snownee <1850986885@qq.com>
2018-11-18 15:45:58 +0800

    Chopping recipes should check config

--- src/main/java/snownee/cuisine/plugins/RusticCompat.java
+++ src/main/java/snownee/cuisine/plugins/RusticCompat.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import snownee.cuisine.Cuisine;
+import snownee.cuisine.CuisineConfig;
 import snownee.cuisine.api.CulinaryHub;
 import snownee.cuisine.api.Form;
 import snownee.cuisine.api.Material;
@@ -29,12 +30,15 @@ public class RusticCompat implements IModule
         Material wildberry = CulinaryHub.API_INSTANCE.register(new SimpleMaterialImpl("wildberry", 0x4D1F23, 0, 0, 0, 0, 0, MaterialCategory.FRUIT).setValidForms(Form.JUICE_ONLY));
         CulinaryHub.API_INSTANCE.registerMapping("cropWildberry", wildberry);
 
-        Item log = ForgeRegistries.ITEMS.getValue(new ResourceLocation("rustic", "log"));
-        Item planks = ForgeRegistries.ITEMS.getValue(new ResourceLocation("rustic", "planks"));
-        if (log != null && planks != null)
+        if (CuisineConfig.GENERAL.axeChopping)
         {
-            Processing.CHOPPING.add(new Chopping(ItemDefinition.of(log, 0), new ItemStack(planks, 6, 0)));
-            Processing.CHOPPING.add(new Chopping(ItemDefinition.of(log, 1), new ItemStack(planks, 6, 1)));
+            Item log = ForgeRegistries.ITEMS.getValue(new ResourceLocation("rustic", "log"));
+            Item planks = ForgeRegistries.ITEMS.getValue(new ResourceLocation("rustic", "planks"));
+            if (log != null && planks != null)
+            {
+                Processing.CHOPPING.add(new Chopping(ItemDefinition.of(log, 0), new ItemStack(planks, 6, 0)));
+                Processing.CHOPPING.add(new Chopping(ItemDefinition.of(log, 1), new ItemStack(planks, 6, 1)));
+            }
         }
     }
 }
