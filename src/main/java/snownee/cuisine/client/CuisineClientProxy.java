bc00f13644cbaf029838aa21dc7811e0bc0f05e4
3TUSK <urey.s.knowledge@gmail.com>
2018-11-17 18:32:42 -0800

    Guess that's enough docs for today

--- src/main/java/snownee/cuisine/client/CuisineClientProxy.java
+++ src/main/java/snownee/cuisine/client/CuisineClientProxy.java
@@ -12,24 +12,52 @@ import snownee.cuisine.CuisineSidedProxy;
 public final class CuisineClientProxy extends CuisineSidedProxy
 {
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical client, this method is delegated to {@link
+     * ModelLoaderRegistry#loadASM(ResourceLocation, ImmutableMap)}.
+     */
     @Override
     public final IAnimationStateMachine loadAnimationStateMachine(ResourceLocation identifier, ImmutableMap<String, ITimeValue> parameters)
     {
         return ModelLoaderRegistry.loadASM(identifier, parameters);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical client, this method is delegate to {@link
+     * net.minecraft.client.resources.I18n}.
+     */
     @Override
     public final String translate(String translationKey)
     {
         return I18n.format(translationKey);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical client, this method is delegate to {@link
+     * net.minecraft.client.resources.I18n}.
+     */
     @Override
     public final String translate(String translationKey, Object... params)
     {
         return I18n.format(translationKey, params);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical client, this method is delegate to {@link
+     * net.minecraft.client.resources.I18n}.
+     */
     @Override
     public boolean canTranslate(String translationKey)
     {
--- src/main/java/snownee/cuisine/server/CuisineServerProxy.java
+++ src/main/java/snownee/cuisine/server/CuisineServerProxy.java
@@ -9,12 +9,32 @@ import snownee.cuisine.CuisineSidedProxy;
 
 public final class CuisineServerProxy extends CuisineSidedProxy
 {
+
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * Since physical server is not responsible for rendering at all, this
+     * implementation will always return {@code null}.
+     *
+     * @return null, always
+     */
     @Override
     public final IAnimationStateMachine loadAnimationStateMachine(ResourceLocation identifier, ImmutableMap<String, ITimeValue> parameters)
     {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical server, this method is delegate to the deprecated
+     * {@code net.minecraft.util.text.translation.I18n}. Note that
+     * that class no longer exists since Minecraft 1.13 Release; as such,
+     * this method will return {@code translationKey} verbatim when it is
+     * ported to Minecraft 1.13 Release or onward.
+     */
     @Override
     @SuppressWarnings("deprecation") // net.minecraft.util.text.translation.I18n is deprecated.
     public final String translate(String translationKey)
@@ -22,6 +42,16 @@ public final class CuisineServerProxy extends CuisineSidedProxy
         return I18n.translateToLocal(translationKey);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical server, this method is delegate to the deprecated
+     * {@code net.minecraft.util.text.translation.I18n}. Note that
+     * that class no longer exists since Minecraft 1.13 Release; as such,
+     * this method will return {@code translationKey} verbatim when it is
+     * ported to Minecraft 1.13 Release or onward.
+     */
     @Override
     @SuppressWarnings("deprecation") // net.minecraft.util.text.translation.I18n is deprecated.
     public final String translate(String translationKey, Object... params)
@@ -29,9 +59,19 @@ public final class CuisineServerProxy extends CuisineSidedProxy
         return I18n.translateToLocalFormatted(translationKey, params);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec
+     * On physical server, this method is delegate to the deprecated
+     * {@code net.minecraft.util.text.translation.I18n}. Note that
+     * that class no longer exists since Minecraft 1.13 Release; as such,
+     * this method will always return {@code false} when it is ported to
+     * Minecraft 1.13 Release or onward.
+     */
     @Override
     @SuppressWarnings("deprecation")
-    public boolean canTranslate(String translationKey)
+    public final boolean canTranslate(String translationKey)
     {
         return I18n.canTranslate(translationKey);
     }
