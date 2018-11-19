bf7739689746904338b84a95021766bf92af9da5
Snownee <1850986885@qq.com>
2018-11-19 14:18:23 +0800

    Close #14

--- src/main/java/snownee/cuisine/blocks/BlockPlacedDish.java
+++ src/main/java/snownee/cuisine/blocks/BlockPlacedDish.java
@@ -7,6 +7,7 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -18,6 +19,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import snownee.cuisine.Cuisine;
@@ -40,6 +44,21 @@ public class BlockPlacedDish extends BlockMod
         setCreativeTab(Cuisine.CREATIVE_TAB);
         setResistance(5.0F);
         setDefaultState(this.blockState.getBaseState().withProperty(MODEL, ModelType.EMPTY));
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onItemRightClick(PlayerInteractEvent.RightClickBlock event)
+    {
+        EntityPlayer player = event.getEntityPlayer();
+        if (player.isCreative() || player.isSneaking())
+        {
+            return;
+        }
+        if (event.getItemStack().getItem() == Item.getItemFromBlock(this))
+        {
+            event.setCanceled(true);
+        }
     }
 
     @Override
