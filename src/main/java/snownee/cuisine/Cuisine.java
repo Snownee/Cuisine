4263e1de01516b60ff7907db1505590b600cd963
3TUSK <urey.s.knowledge@gmail.com>
2018-12-12 02:02:01 -0800

    Nuke re-invented wheel and duplicated dependency info

--- src/main/java/snownee/cuisine/Cuisine.java
+++ src/main/java/snownee/cuisine/Cuisine.java
@@ -40,9 +40,7 @@ import snownee.kiwi.util.VariantsHolder;
         name = Cuisine.NAME,
         version = "@VERSION_INJECT@",
         useMetadata = true,
-        acceptedMinecraftVersions = "[1.12, 1.13)",
-        guiFactory = "snownee.cuisine.client.CuisineConfigGUI",
-        dependencies = "required-after:kiwi@[0.3, 0.4);"
+        acceptedMinecraftVersions = "[1.12, 1.13)"
 )
 public class Cuisine
 {
