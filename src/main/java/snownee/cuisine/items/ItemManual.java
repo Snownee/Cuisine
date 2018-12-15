64ed0e91d1d6aa656c2299d28221fce55f7d882f
3TUSK <urey.s.knowledge@gmail.com>
2018-12-15 13:47:01 -0800

    Deprecate the native manual item, mark SpawnHandler as pending evaluation

--- src/main/java/snownee/cuisine/items/ItemManual.java
+++ src/main/java/snownee/cuisine/items/ItemManual.java
@@ -1,14 +1,9 @@
 package snownee.cuisine.items;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.world.World;
 import snownee.cuisine.Cuisine;
 import snownee.kiwi.item.ItemMod;
 
+@Deprecated
 public class ItemManual extends ItemMod
 {
 
@@ -17,16 +12,6 @@ public class ItemManual extends ItemMod
         super(name);
         setMaxStackSize(1);
         setCreativeTab(Cuisine.CREATIVE_TAB);
-    }
-
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
-    {
-        ItemStack heldThing = player.getHeldItem(hand);
-        /*player.openGui(Cuisine.getInstance(), CuisineGUI.MANUAL, world, hand == EnumHand.MAIN_HAND
-                ? player.inventory.currentItem
-                : player.inventory.mainInventory.size() + player.inventory.armorInventory.size(), 0, 0);*/
-        return new ActionResult<>(EnumActionResult.SUCCESS, heldThing);
     }
 
 }
