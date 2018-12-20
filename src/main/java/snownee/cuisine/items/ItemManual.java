8d01d8f9bcff406fc39622e8e84275a8538e747f
3TUSK <urey.s.knowledge@gmail.com>
2018-12-19 23:40:05 -0800

    Snownee asks for keeping this. So... here we go... strategy pattern...

--- src/main/java/snownee/cuisine/items/ItemManual.java
+++ src/main/java/snownee/cuisine/items/ItemManual.java
@@ -1,17 +1,56 @@
 package snownee.cuisine.items;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 import snownee.cuisine.Cuisine;
+import snownee.cuisine.client.gui.CuisineGUI;
 import snownee.kiwi.item.ItemMod;
 
-@Deprecated
+import java.util.Objects;
+
 public class ItemManual extends ItemMod
 {
+
+    private OpenManualHandler manualHandler;
 
     public ItemManual(String name)
     {
         super(name);
-        setMaxStackSize(1);
-        setCreativeTab(Cuisine.CREATIVE_TAB);
+        this.setMaxStackSize(1);
+        this.setCreativeTab(Cuisine.CREATIVE_TAB);
     }
 
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
+    {
+        if (this.manualHandler == null)
+        {
+            return defaultManualHandler(world, player, hand);
+        }
+        else
+        {
+            return this.manualHandler.tryOpenManual(world, player, hand);
+        }
+    }
+
+    private static ActionResult<ItemStack> defaultManualHandler(World world, EntityPlayer player, EnumHand hand)
+    {
+        player.openGui(Cuisine.getInstance(), CuisineGUI.MANUAL, world, hand.ordinal(), 0, 0);
+        return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+    }
+
+    public final ItemManual setOpenManualHandler(OpenManualHandler handler)
+    {
+        this.manualHandler = Objects.requireNonNull(handler, "Manual handler cannot be null");
+        return this;
+    }
+
+    public interface OpenManualHandler
+    {
+        ActionResult<ItemStack> tryOpenManual(World world, EntityPlayer player, EnumHand hand);
+    }
 }
--- src/main/java/snownee/cuisine/plugins/patchouli/ManualSupport.java
+++ src/main/java/snownee/cuisine/plugins/patchouli/ManualSupport.java
@@ -1,7 +1,11 @@
 package snownee.cuisine.plugins.patchouli;
 
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.ResourceLocation;
 import snownee.cuisine.Cuisine;
 import snownee.cuisine.CuisineConfig;
+import snownee.cuisine.CuisineRegistry;
 import snownee.kiwi.IModule;
 import snownee.kiwi.KiwiModule;
 import vazkii.patchouli.api.PatchouliAPI;
@@ -13,6 +17,12 @@ public class ManualSupport implements IModule
     @Override
     public void init()
     {
+        CuisineRegistry.MANUAL.setOpenManualHandler((world, player, hand) ->
+        {
+           PatchouliAPI.instance.openBookGUI(new ResourceLocation(Cuisine.MODID, "culinary_101"));
+           return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+        });
+
         PatchouliAPI.instance.setConfigFlag("cuisine:enable_axe_chopping", CuisineConfig.GENERAL.axeChopping);
     }
 }
