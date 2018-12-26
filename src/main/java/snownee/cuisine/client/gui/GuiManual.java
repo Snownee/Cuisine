e8ed8daac2f2cf2a2137b7f84830f4ef984f8981
Snownee <mjy523@126.com>
2018-12-27 03:34:04 +0800

    Remove unused codes. Update default manual

--- src/main/java/snownee/cuisine/client/gui/GuiManual.java
+++ src/main/java/snownee/cuisine/client/gui/GuiManual.java
@@ -2,8 +2,8 @@ package snownee.cuisine.client.gui;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiConfirmOpenLink;
@@ -12,8 +12,6 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import snownee.cuisine.Cuisine;
-import snownee.cuisine.api.CulinarySkill;
-import snownee.cuisine.api.CulinarySkillManager;
 import snownee.cuisine.util.I18nUtil;
 import snownee.kiwi.client.AdvancedFontRenderer;
 import snownee.kiwi.client.FontUtil;
@@ -31,22 +29,11 @@ public class GuiManual extends GuiScreen
     public final DrawableNineSlice pageGrid;
     private boolean twoPages = false;
 
-    private int currentSkillPage = 1;
-    private int maxSkillPages = CulinarySkillManager.getSkills().size() / 4;
-    private GuiButton buttonSkillNextPage;
-    private GuiButton buttonSkillPreviousPage;
-
-    private GuiButton buttonNextPage;
-    private GuiButton buttonPreviousPage;
-    private GuiButton buttonDone;
-
-    private final ItemStack stack;
-
     private final String chatRoomURL = I18nUtil.translate("gui.chat_room_link");
+    private final String mcmodWikiURL = I18nUtil.translate("gui.mcmod_link");
 
     public GuiManual(int slot, ItemStack stack)
     {
-        this.stack = stack;
         pageGrid = new DrawableNineSlice(BOOK_GUI_TEXTURES, 0, 0, 75, 75, 25, 25, 25, 25);
         pageGrid.setHeight(PAGE_HEIGHT);
         pageGrid.setWidth(PAGE_WIDTH);
@@ -57,10 +44,14 @@ public class GuiManual extends GuiScreen
     {
         this.fontRenderer = AdvancedFontRenderer.INSTANCE;
         this.buttonList.clear();
-        this.buttonNextPage = this.addButton(new GuiButton(0, (this.width + PAGE_WIDTH - 80/* + SKILL_PANEL_WIDTH*/) / 2, (this.height - 20) / 2 - 20, 80, 20, I18nUtil.translate("gui.openlink")));
-        this.buttonDone = this.addButton(new GuiButton(1, (this.width + PAGE_WIDTH - 80/* + SKILL_PANEL_WIDTH*/) / 2, (this.height - 20) / 2 + 20, 80, 20, I18n.format("gui.close"))); // Use vanilla language key
-        //this.buttonSkillNextPage = this.addButton(new GuiButton(2, (this.width - getXSize()) / 2 - 10, (this.height - getYSize()) / 2 + PAGE_HEIGHT - 10, 10, 10, ">"));
-        //this.buttonSkillPreviousPage = this.addButton(new GuiButton(3, (this.width - getXSize()) / 2 - SKILL_PANEL_WIDTH, (this.height - getYSize()) / 2 + PAGE_HEIGHT - 10, 10, 10, "<"));
+        this.addButton(new GuiButton(0, (this.width + PAGE_WIDTH - 80) / 2, (this.height - 20) / 2, 80, 20, I18nUtil.translate("gui.openlink")));
+        this.addButton(new GuiButton(1, (this.width + PAGE_WIDTH - 80) / 2, (this.height - 20) / 2 + 40, 80, 20, I18n.format("gui.close"))); // Use vanilla language key
+
+        Locale locale = mc.getLanguageManager().getCurrentLanguage().getJavaLocale();
+        if (locale.toString().startsWith("zh"))
+        {
+            this.addButton(new GuiButton(2, (this.width + PAGE_WIDTH - 80) / 2, (this.height - 20) / 2 - 40, 80, 20, I18nUtil.translate("gui.openwiki")));
+        }
     }
 
     @Override
@@ -69,45 +60,20 @@ public class GuiManual extends GuiScreen
         // NO-OP
     }
 
-    private void drawSkillInfo()
-    {
-        int u = (this.width - getXSize()) / 2 + 5;
-        int v = (this.height - getYSize()) / 2 + 10;
-
-        List<CulinarySkill> skills = new ArrayList<>(CulinarySkillManager.getSkills());
-        int s = (currentSkillPage - 1) * 4;
-        for (int i = 0; i < 4 && s + i < skills.size(); i++)
-        {
-            String skill = skills.get(s + i).getName();
-            String name = I18n.format(skills.get(s + i).getTranslationKey());
-            int level = 123;// TODO get level
-            FontUtil.drawSplitStringOverflow(fontRenderer, skill + ": " + 123, u, v + 30 * i, SKILL_PANEL_WIDTH - 20, PAGE_HEIGHT, 0, false);
-        }
-    }
-
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
-        //        PAGE_WIDTH = mouseX + PAGE_MARGIN * 2;
-        //        PAGE_HEIGHT = mouseY + PAGE_MARGIN * 2;
-
         twoPages = true;
         mc.getTextureManager().bindTexture(BOOK_GUI_TEXTURES);
         int i = (this.width - getXSize()) / 2;
         int j = (this.height - getYSize()) / 2;
-        pageGrid.draw(mc, i/* + SKILL_PANEL_WIDTH*/, j);
+        pageGrid.draw(mc, i, j);
         if (twoPages)
         {
-            pageGrid.draw(mc, i + PAGE_WIDTH/* + SKILL_PANEL_WIDTH*/, j);
+            pageGrid.draw(mc, i + PAGE_WIDTH, j);
         }
 
-        //mc.getTextureManager().bindTexture(skillPanel);
-        //pageGrid.draw(i, j, SKILL_PANEL_WIDTH, PAGE_HEIGHT, false, false);
-        //this.drawTexturedModalRect(i, j, 0, 0, SKILL_PANEL_WIDTH, PAGE_HEIGHT); // Same below
-
-        //drawSkillInfo(); // TODO (3TUSK): DO NOT USE UNLESS YOU ARE PART OF CUISINE DEV TEAM
-
-        int originX = i + PAGE_MARGIN/* + SKILL_PANEL_WIDTH*/; // Same above
+        int originX = i + PAGE_MARGIN;
         int originY = j + PAGE_MARGIN;
 
         String str = I18nUtil.translateWithEscape("gui.welcome");
@@ -123,7 +89,7 @@ public class GuiManual extends GuiScreen
 
     private int getXSize()
     {
-        return (twoPages ? PAGE_WIDTH + PAGE_WIDTH : PAGE_WIDTH);// + SKILL_PANEL_WIDTH;
+        return (twoPages ? PAGE_WIDTH + PAGE_WIDTH : PAGE_WIDTH);
     }
 
     private int getYSize()
@@ -164,17 +130,9 @@ public class GuiManual extends GuiScreen
             }
             else if (button.id == 2)
             {
-                if (currentSkillPage < maxSkillPages)
-                {
-                    currentSkillPage++;
-                }
-            }
-            else if (button.id == 3)
-            {
-                if (currentSkillPage > 1)
-                {
-                    currentSkillPage--;
-                }
+                GuiConfirmOpenLink guiConfirm = new GuiConfirmOpenLink(this, mcmodWikiURL, 4, true);
+                guiConfirm.disableSecurityWarning();
+                mc.displayGuiScreen(guiConfirm);
             }
         }
     }
@@ -182,11 +140,11 @@ public class GuiManual extends GuiScreen
     @Override
     public void confirmClicked(boolean result, int id)
     {
-        if (result && id == 3)
+        if (result && (id == 3 || id == 4))
         {
             try
             {
-                java.awt.Desktop.getDesktop().browse(new URI(chatRoomURL));
+                java.awt.Desktop.getDesktop().browse(new URI(id == 3 ? chatRoomURL : mcmodWikiURL));
             }
             catch (URISyntaxException wrongURI)
             {
