0dfea13e730de274d62cd8e28adacba9c4cbed55
3TUSK <urey.s.knowledge@gmail.com>
2018-12-28 22:26:59 -0800

    This should not expose to outside world.

--- src/main/java/snownee/cuisine/plugins/crafttweaker/CTIngredientInput.java
+++ src/main/java/snownee/cuisine/plugins/crafttweaker/CTIngredientInput.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public final class CTIngredientInput implements ProcessingInput
+final class CTIngredientInput implements ProcessingInput
 {
     private final IIngredient ingredient;
 
