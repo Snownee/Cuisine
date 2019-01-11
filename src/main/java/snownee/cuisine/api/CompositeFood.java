2973d9f4f520cf0fb4712b4350b82376bf7ad3e1
Snownee <mjy523@126.com>
2019-01-12 00:28:51 +0800

    Add some punishments for bad cooking skill

--- src/main/java/snownee/cuisine/api/CompositeFood.java
+++ src/main/java/snownee/cuisine/api/CompositeFood.java
@@ -16,7 +16,10 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import snownee.cuisine.api.CulinaryHub.CommonEffects;
@@ -323,7 +326,26 @@ public abstract class CompositeFood
 
         collector.apply(this, player);
 
-        player.getFoodStats().addStats(collector.getNewFoodLevel(), getSaturationModifier());
+        int countOvercooked = (int) getIngredients().stream().filter(i -> i.getAllTraits().contains(IngredientTrait.OVERCOOKED)).count();
+        int countPlain = (int) getIngredients().stream().filter(i -> i.getAllTraits().contains(IngredientTrait.PLAIN) || i.getAllTraits().contains(IngredientTrait.UNDERCOOKED)).count();
+        int newFoodLevel = collector.getNewFoodLevel() - countOvercooked;
+        if (newFoodLevel > 0)
+        {
+            player.getFoodStats().addStats(newFoodLevel, getSaturationModifier());
+        }
+        if (!worldIn.isRemote)
+        {
+            if (countPlain / (float) getIngredients().size() > 0.8F)
+            {
+                Potion potion = worldIn.rand.nextBoolean() ? MobEffects.MINING_FATIGUE : MobEffects.WEAKNESS;
+                player.addPotionEffect(new PotionEffect(potion, 300 * getIngredients().size()));
+            }
+            if (countOvercooked / (float) getIngredients().size() > 0.8F)
+            {
+                Potion potion = worldIn.rand.nextBoolean() ? MobEffects.POISON : MobEffects.NAUSEA;
+                player.addPotionEffect(new PotionEffect(potion, 100 * getIngredients().size()));
+            }
+        }
     }
 
     protected Collection<EffectBinding> getEffectBindings()
