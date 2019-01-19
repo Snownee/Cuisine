2e6b1196d5b05a4a86b0c8d3782a01d480b111aa
Snownee <mjy523@126.com>
2019-01-19 18:14:59 +0800

    Update book

--- src/main/java/snownee/cuisine/plugins/patchouli/PageDrinkType.java
+++ src/main/java/snownee/cuisine/plugins/patchouli/PageDrinkType.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import snownee.cuisine.CuisineRegistry;
 import snownee.cuisine.internal.food.Drink;
 import snownee.cuisine.internal.food.Drink.DrinkType;
+import snownee.cuisine.util.I18nUtil;
 import snownee.kiwi.crafting.input.ProcessingInput;
 import snownee.kiwi.util.NBTHelper;
 import vazkii.patchouli.client.book.BookEntry;
 import vazkii.patchouli.client.book.BookPage;
+import vazkii.patchouli.client.book.gui.GuiBook;
 
 public class PageDrinkType extends BookPage
 {
@@ -31,14 +36,18 @@ public class PageDrinkType extends BookPage
         if (drinkType != null)
         {
             drink = new ItemStack(CuisineRegistry.DRINK);
-            NBTHelper.of(drink).setString("model", drinkType.getName());
+            NBTHelper.of(drink).setString("model", drinkType.getName()).setInt("liquidColor", 0xF08A19);
             containers = drinkType.getContainerPre().examples();
             featureInputs = new ArrayList<>();
             for (Entry<ProcessingInput, DrinkType> e : Drink.Builder.FEATURE_INPUTS.entrySet())
             {
                 if (e.getValue() == drinkType)
                 {
-                    featureInputs.add(e.getKey().examples());
+                    List<ItemStack> examples = e.getKey().examples();
+                    if (!examples.isEmpty())
+                    {
+                        featureInputs.add(examples);
+                    }
                 }
             }
         }
@@ -51,7 +60,51 @@ public class PageDrinkType extends BookPage
         {
             return;
         }
-        parent.getFont().drawString(I18n.format(drinkType.getTranslationKey() + ".name"), 0, 0, book.textColor);
+        parent.drawCenteredStringNoShadow(I18n.format(drinkType.getTranslationKey() + ".name"), GuiBook.PAGE_WIDTH / 2, -4, book.textColor);
+
+        int w = 68;
+        int h = 26;
+        mc.renderEngine.bindTexture(book.craftingResource);
+        GlStateManager.enableBlend();
+        Gui.drawModalRectWithCustomSizedTexture(GuiBook.PAGE_WIDTH / 2 - w / 2, 10, 0, 128 - h, w, h, 128, 128);
+
+        parent.renderItemStack(GuiBook.PAGE_WIDTH / 2 - 9, 15, mouseX, mouseY, drink);
+
+        if (drinkType != DrinkType.NORMAL)
+        {
+            parent.getFont().drawString(I18nUtil.translate("gui.featureInput"), 4, 40, book.textColor);
+        }
+        parent.getFont().drawString(I18nUtil.translate("gui.container"), 4, drinkType == DrinkType.NORMAL ? 40 : 80, book.textColor);
+        parent.getFont().drawString(I18nUtil.translate("gui.potionVege", I18n.format(drinkType.getPotionVege().getName())), 4, 120, book.textColor);
+        parent.getFont().drawString(I18nUtil.translate("gui.potionFruit", I18n.format(drinkType.getPotionFruit().getName())), 4, 130, book.textColor);
+
+        int x = 0;
+        for (List<ItemStack> inputs : featureInputs)
+        {
+            drawItems(x, 48, mouseX, mouseY, inputs);
+            x += 24;
+        }
+        drawItems(0, drinkType == DrinkType.NORMAL ? 48 : 88, mouseX, mouseY, containers);
+    }
+
+    private void drawItems(int x, int y, int mouseX, int mouseY, List<ItemStack> items)
+    {
+        if (items.isEmpty())
+        {
+            return;
+        }
+        drawItem(x, y, mouseX, mouseY, items.get(items.size() == 1 ? 0 : (int) (Minecraft.getSystemTime() / 1000 % items.size())));
+    }
+
+    private void drawItem(int x, int y, int mouseX, int mouseY, ItemStack item)
+    {
+        int size = 26;
+
+        mc.renderEngine.bindTexture(book.craftingResource);
+        GlStateManager.enableBlend();
+        Gui.drawModalRectWithCustomSizedTexture(x, y, 20, 128 - size, size, size, 128, 128);
+
+        parent.renderItemStack(x + 5, y + 5, mouseX, mouseY, item);
     }
 
 }
