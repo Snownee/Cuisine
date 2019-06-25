2f91f4ee053800d1cdd6ec70ff267f7eca51e4e6
Snownee <mjy523@126.com>
2019-06-26 02:01:44 +0800

    Fix sheared leaeves not passable

--- src/main/java/snownee/cuisine/blocks/BlockShearedLeaves.java
+++ src/main/java/snownee/cuisine/blocks/BlockShearedLeaves.java
@@ -10,6 +10,8 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -17,6 +19,7 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -39,6 +42,16 @@ public class BlockShearedLeaves extends BlockMod implements IShearable, IGrowabl
         this.setHardness(0.2F);
         this.setLightOpacity(1);
         setDefaultState(blockState.getBaseState().withProperty(FLOWER, false).withProperty(BlockModSapling.VARIANT, BlockModSapling.Type.POMELO));
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean isActualState)
+    {
+        if (!BlockModLeaves.passable && entityIn != null && !(entityIn instanceof EntityItem))
+        {
+            super.addCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn, isActualState);
+        }
     }
 
     @Override
