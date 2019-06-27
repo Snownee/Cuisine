6f81bccddd8321e3a9471f85cc01818b6be687f8
Snownee <mjy523@126.com>
2019-06-27 18:30:21 +0800

    Fix DisableGenericFood

--- src/main/java/snownee/cuisine/events/DisableGenericFood.java
+++ src/main/java/snownee/cuisine/events/DisableGenericFood.java
@@ -3,27 +3,31 @@ package snownee.cuisine.events;
 import net.minecraft.block.BlockCake;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemFood;
+import net.minecraft.util.EnumActionResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class DisableGenericFood
 {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public void onRightClickItem(PlayerInteractEvent.RightClickItem event)
     {
         if (event.getItemStack().getItem() instanceof ItemFood)
         {
             event.setCanceled(true);
+            event.setCancellationResult(EnumActionResult.PASS);
         }
     }
-    
-    @SubscribeEvent
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
     {
         IBlockState state = event.getWorld().getBlockState(event.getPos());
         if (state.getBlock() instanceof BlockCake)
         {
             event.setCanceled(true);
+            event.setCancellationResult(EnumActionResult.PASS);
         }
     }
 }
